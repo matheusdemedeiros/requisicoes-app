5d2e7bf9ddcf6938391b56b4f3c9346f171ffb5e
Matheus <matheusdemedeiros200599@gmail.com>
2022-08-26 17:37:59 -0300

    feat: parte inicial e roteamento básico.

---
+++ src/app/auth/services/authentication.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
+import { Observable, ObservedValueOf } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthenticationService {
+  public usuarioLogado: Observable<firebase.User | null>;
+
+  constructor(private auth: AngularFireAuth) {
+    this.usuarioLogado = auth.authState;
+  }
+
+  public login(
+    email: string,
+    senha: string
+  ): Promise<firebase.auth.UserCredential> {
+    return this.auth.signInWithEmailAndPassword(email, senha);
+  }
+
+  public logout():Promise<void>{
+    return this.auth.signOut();
+  }
+
+  public resetarsenha(email: string): Promise<void> {
+    return this.auth.sendPasswordResetEmail(email);
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey : "AIzaSyDl7XBneCu9LBeBX046L_wk9eyjtpKsVHo",
+    authDomain: "requisicoes-app-4428c.firebaseapp.com",
+    projectId: "requisicoes-app-4428c",
+    storageBucket: "requisicoes-app-4428c.appspot.com",
+    messagingSenderId: "292712459026",
+    appId: "1:292712459026:web:2243efdae8e623e24d21cb"
+  }
 };
 
 /*
