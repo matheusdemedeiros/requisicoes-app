1289f0760099b82220210428047d5f361cae5d3d
Matheus <matheusdemedeiros200599@gmail.com>
2022-09-14 10:43:10 -0300

    alterações de configuração

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey : "AIzaSyDl7XBneCu9LBeBX046L_wk9eyjtpKsVHo",
+    authDomain: "requisicoes-app-4428c.firebaseapp.com",
+    projectId: "requisicoes-app-4428c",
+    storageBucket: "requisicoes-app-4428c.appspot.com",
+    messagingSenderId: "292712459026",
+    appId: "1:292712459026:web:2243efdae8e623e24d21cb"
+  }
 };
